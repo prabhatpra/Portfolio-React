3584bc8f1c1af3c1600a35a949af5e981e18413c
prabhat prajapati <prabhatprajapati01@gmail.com>
2026-04-30 21:35:51 +0530

    moblic menu dark text color change

--- frontend/Portfolio-Dis/src/Component/Navbar/Navbar.jsx
+++ frontend/Portfolio-Dis/src/Component/Navbar/Navbar.jsx
@@ -144,9 +144,9 @@ const Navbar = () => {
               <div className="md:hidden">
                 <button onClick={() => setIsOpen((prev) => !prev)}>
                   {isOpen ? (
-                    <HiX className="w-6 h-6" />
+                    <HiX className="w-6 h-6 dark:text-white" />
                   ) : (
-                    <HiMenuAlt3 className="w-6 h-6 hover:text-green-500 transition" />
+                    <HiMenuAlt3 className="w-6 h-6 dark:text-white  transition" />
                   )}
                 </button>
               </div>
@@ -164,7 +164,7 @@ const Navbar = () => {
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
-            className="md:hidden absolute right-4 top-[60px] w-48 bg-white dark:bg-gray-900 rounded-lg shadow-md p-2 z-50"
+            className="md:hidden absolute right-4 top-[60px] w-48 bg-white dark:bg-gray-900 dark:text-red-500 rounded-lg shadow-md p-2 z-50"
           >
             {menuItems.map((item, i) =>
               item.link.startsWith("#") ? (
